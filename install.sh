c3e6a931cd79fa8f3289261202a78820e48053aa
shun-sfoo <ganymede0915@gmail.com>
2021-05-29 14:10:26 +0800

    little

--- install.sh
+++ install.sh
@@ -3,9 +3,5 @@
 #   - download binary
 #   - build with cargo
 
-set -o nounset    # error when referencing undefined variable
-set -o errexit    # exit when command fails
-
 echo "hello rust"
-
 echo "hello rust" >> $HOME/test.txt
