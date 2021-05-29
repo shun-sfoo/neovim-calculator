217a3825e7b11d506aa4d7d710bc6dcb5a96504e
shun-sfoo <ganymede0915@gmail.com>
2021-05-29 14:31:14 +0800

    try

--- install.sh
+++ install.sh
@@ -3,5 +3,22 @@
 #   - download binary
 #   - build with cargo
 
-echo "hello rust"
-echo "hello rust" >> $HOME/test.txt
+set -o nounset    # error when referencing undefined variable
+set -o errexit    # exit when command fails
+
+cargo_build() {
+    if command -v cargo > /dev/null; then
+        echo "Trying to build locally using Cargo.."
+        cargo build --release
+    else
+        echo "Could not build binary. Your installation might be corrupt."
+        return 1
+    fi
+}
+
+arch=$(uname -sm)
+
+case "${arch}" in
+    "Darwin x86_64") echo "No pre-built binary available for ${arch}."; cargo_build ;;
+    *) echo "No pre-built binary available for ${arch}."; cargo_build ;;
+esac
